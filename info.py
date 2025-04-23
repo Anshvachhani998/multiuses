10caaa1077f2dc852e903ee229a21ea370acac19
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-23 13:42:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ import os
 SESSION = "teraboxdl"
 API_ID = int(os.getenv("API_ID", "22141398"))
 API_HASH = os.getenv("API_HASH", "0c8f8bd171e05e42d6f6e5a6f4305389")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7277194738:AAHrewQsvKcPqeXYeMIbSk-nyUjgJ14kW8U")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6538627123:AAEy4JiDRG_OrA3AJn8t-YIKy_N_3Z6BsNc")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002284232975"))
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002284232975"))
 PORT = int(os.getenv("PORT", "8080"))
