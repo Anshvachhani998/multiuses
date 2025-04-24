8995d46562d34d4e8d42f79428315a29d0ca48ba
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-24 19:25:22 +0530

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -322,6 +322,7 @@ async def google_drive(client, chat_id, gdrive_url):
 
             # Final download link for aria2c
             download_url = await get_confirm_token_download_url(file_id)
+            
             logging.info(download_url)
             final_filenames = await asyncio.to_thread(
                 aria2c_download,
