9b27cf9e281dae849694f52ec0781c4121cbff9b
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-24 15:22:47 +0530

    Update cmd.py

--- plugins/cmd.py
+++ plugins/cmd.py
@@ -15,8 +15,7 @@ from pyrogram.enums import ParseMode
 logger = logging.getLogger(__name__)
 
  
-
-
+  
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     buttons = InlineKeyboardMarkup([
