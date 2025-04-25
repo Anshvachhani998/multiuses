d6b168e71828f5de1a3fa807708f997d5ec42145
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-25 11:58:58 +0530

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -437,7 +437,7 @@ def gdown_download(url, download_dir, label, queue, client):
         # Return the most recent downloaded file
         files.sort(key=lambda x: os.path.getmtime(os.path.join(download_dir, x)), reverse=True)
         final_path = os.path.join(download_dir, files[0])
-        print(f"GDOWN name {final_path}")
+        logging.info(f"GDOWN name {final_path}")
         return final_path
 
     except Exception as e:
