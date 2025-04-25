368211d7096bbdeb651c4d8d0d1cb28b3e245235
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-25 23:32:18 +0530

    Update dwn.py

--- plugins/dwn.py
+++ plugins/dwn.py
@@ -6,6 +6,11 @@ from yt_dlp import YoutubeDL
 from pyrogram import Client, filters
 from plugins.download import download_video, aria2c_media, google_drive
 
+import pickle
+import re
+from googleapiclient.discovery import build
+from pyrogram import Client, filters
+
 
 DOWNLOAD_DIR = "downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
