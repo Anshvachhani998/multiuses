df1885cbf7f1dc21e4e09d041cee28217ab4b8cc
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-26 10:40:28 +0530

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -408,12 +408,14 @@ import logging
 
 def gdown_download(url, download_dir, filename, queue, client):
     try:
+        os.makedirs(download_dir, exist_ok=True)
+        path = os.path.join(download_dir, filename)
         cmd = [
             "gdown",
             url,
             "--fuzzy",
             "--no-cookies",
-            "--output", os.path.join(download_dir, filename)
+            "--output", path
         ]
 
         process = subprocess.Popen(
