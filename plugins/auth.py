85d31d7880abcd40c40010ee59b78f3b544946fd
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-30 15:45:45 +0530

    Update auth.py

--- plugins/auth.py
+++ plugins/auth.py
@@ -4,11 +4,8 @@ import pickle
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
 
-# Old (causing error)
-creds = flow.run_local_server(port=0)
-
-# ✅ New (Console-based, CLI-safe)
-#creds = flow.run_console()
+# Use this for VPS/Terminal environments
+creds = flow.run_console()
 
 with open("token.pickle", "wb") as token:
     pickle.dump(creds, token)
