8a3efff8d6cb5a6c89c9ec545a8c8826786a99f8
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-30 16:07:05 +0530

    Update auth.py

--- plugins/auth.py
+++ plugins/auth.py
@@ -2,10 +2,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 import pickle
 
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
 
-flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-creds = flow.run_console()
+# Use `run_local_server()` to authenticate
+creds = flow.run_local_server(port=0)
 
+# Save the credentials
 with open('token.pickle', 'wb') as token:
     pickle.dump(creds, token)
 
