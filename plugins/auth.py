26abaa6df957d10842a4027545b9539e503d536b
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-30 15:43:47 +0530

    Update auth.py

--- plugins/auth.py
+++ plugins/auth.py
@@ -5,10 +5,10 @@ SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
 
 # Old (causing error)
-# creds = flow.run_local_server(port=0)
+creds = flow.run_local_server(port=0)
 
 # ✅ New (Console-based, CLI-safe)
-creds = flow.run_console()
+#creds = flow.run_console()
 
 with open("token.pickle", "wb") as token:
     pickle.dump(creds, token)
