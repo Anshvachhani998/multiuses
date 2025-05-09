a9c1b2f65a737d938939f836eb9a464bf7ec6304
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-09 17:14:18 +0530

    Update auth.py

--- plugins/auth.py
+++ plugins/auth.py
@@ -2,16 +2,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 # Replace with your own credentials file path
 CLIENT_SECRET_FILE = 'credentials.json'
-
-# Define the API scopes you need
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
-# Create the flow using the credentials
-flow = InstalledAppFlow.from_client_secrets_file(
-    CLIENT_SECRET_FILE, SCOPES)
+flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
 
-# Run the local server to authorize
-creds = flow.run_local_server(port=0)
+# Run the console flow, it will give a URL to visit
+creds = flow.run_console()
 
 # Save the credentials for later use
 with open('token.pickle', 'wb') as token:
