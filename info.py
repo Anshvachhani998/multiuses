2efd7d5121d619427c1c10da0de10a4ac1bc2b96
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-07-10 15:03:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ PORT = int(os.getenv("PORT", "8080"))
 FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "-1002858808929"))
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Ansh089:Ansh089@cluster0.y8tpouc.mongodb.net/?retryWrites=true&w=majority")
 MONGO_NAME = os.getenv("MONGO_NAME", "MUlTI")
-ADMINS = [5660839376, 6167872503, 5961011848]
+ADMINS = [5660839376, 6167872503, 5961011848, 7744665378]
 DAILY_LIMITS = 20
 MAINTENANCE_MODE = False
 
