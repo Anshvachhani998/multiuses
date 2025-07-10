a071249e7bb932fe255e5f1f6a9686c47f2570cf
ftmbotzx <kumarasta91@gmail.com>
2025-07-10 16:21:21 +0530

    Update premium.py

--- plugins/premium.py
+++ plugins/premium.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from database.db import db
-from config import Config
+from info import Config
 import logging
 from datetime import datetime, timedelta
 
