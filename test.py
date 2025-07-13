92cbdad8102466a8f0be115bbaacc2dff4b73dd3
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-07-13 23:09:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import undetected_chromedriver as uc
 
 options = uc.ChromeOptions()
-options.headless = False  # Disable headless for debug
+options.headless = False
+options.binary_location = "/usr/bin/google-chrome"
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-driver = uc.Chrome(options=options)
-driver.get('https://www.google.com')
+driver = uc.Chrome(options=options, use_subprocess=True)
+
+driver.get("https://www.google.com")
 print(driver.title)
+
 driver.quit()
