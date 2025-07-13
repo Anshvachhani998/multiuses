76a1b6529e2ed2c53878fbb6984ea97acccd212b
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-07-13 23:27:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import undetected_chromedriver as uc
 
 options = uc.ChromeOptions()
 options.add_argument("--headless=new")  # headless mode explicitly
-options.binary_location = "/opt/google/chrome/google-chrome"
+options.binary_location = "/opt/google/chrome/chrome"
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 
 try:
     driver = uc.Chrome(options=options, use_subprocess=True)
-    driver.get("https://www.google.com")
+    driver.get("https://spotidown.app/")
     print("Title:", driver.title)
 finally:
     driver.quit()
